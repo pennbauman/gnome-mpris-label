46aad4c899a9378debac68e7815d6ebb5e12c5b9
Penn Bauman <me@pennbauman.com>
2025-04-21 03:40:21 -0700

    Switch patching to make new version the base

--- patches/apply.sh
+++ patches/apply.sh
@@ -16,11 +16,16 @@ if [ ! -d "$DIR" ]; then
 	exit 1
 fi
 
+GNOME_VERSION="$(gnome-shell --version | awk -F'[ .]' '{print $3}')"
 
-if [ "$(gnome-shell --version | awk -F'[ .]' '{print $3}')" -ge 45 ]; then
-	patch -s -d "$DIR" < patches/gnome45-compatibility.patch
-fi
-if [ "$(gnome-shell --version | awk -F'[ .]' '{print $3}')" -ge 47 ]; then
-	patch -s -d "$DIR" < patches/gnome47-compatibility.patch
+if [ "$GNOME_VERSION" -eq 45 ] || [ "$GNOME_VERSION" -eq 46 ]; then
+	echo APPLY patches/gnome-45-46-compatibility.patch
+	patch -s -V never -d "$DIR" < patches/gnome-45-46-compatibility.patch
+elif [ "$GNOME_VERSION" -eq 43 ] || [ "$GNOME_VERSION" -eq 44 ]; then
+	echo APPLY patches/gnome-43-44-compatibility.patch
+	patch -s -V never -d "$DIR" < patches/gnome-43-44-compatibility.patch
+elif [ "$GNOME_VERSION" -lt 43 ]; then
+	echo "GNOME versions below 43 not supported"
+	exit 1
 fi
 
